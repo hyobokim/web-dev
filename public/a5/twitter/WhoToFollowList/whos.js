2b71e9d6fcbe92f59878ef20e9ce9e5bb8c42268
Hyoboem <hyoboemkim1@gmail.com>
2021-10-18 05:24:14 -0400

    Working WhoToFollowList

---
+++ public/a5/twitter/WhoToFollowList/whos.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    avatarIcon : "../../../images/facebook.jpg",
+    userName : 'Facebook',
+    handle : 'Facebook',
+  },
+  {
+    avatarIcon: "../../../images/amazon.jpg",
+    userName : 'Amazon',
+    handle: 'Amazon'
+  }
+];
