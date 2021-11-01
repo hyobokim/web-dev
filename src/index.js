c4f91225d80e063e9743f84905e9e1dee27d2dbc
Hyoboem <hyoboemkim1@gmail.com>
2021-11-01 13:02:16 -0400

    Finished A6

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './vendors/bootstrap/css/bootstrap.min.css';
-import './vendors/fontawesome/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 
 ReactDOM.render(
